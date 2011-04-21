7e68461a6a85c78d53ba6bb1a048a9d22b2a2303
Luis Lavena <luislavena@gmail.com>
2011-04-20 21:44:07 -0300

    Always do requires

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,7 @@ rescue LoadError
   abort "This project requires Isolate to manage it's dependencies. Please `gem install isolate` and try again. Thank you."
 end
 
+require "rspec/core"
+
 RSpec.configure do |config|
 end

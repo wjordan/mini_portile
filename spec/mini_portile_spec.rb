3497aac32ba012e4de99c319866b675356105619
Luis Lavena <luislavena@gmail.com>
2011-04-27 20:14:04 -0300

    Refactor specs to make it more easy to use helpers

--- spec/mini_portile_spec.rb
+++ spec/mini_portile_spec.rb
@@ -2,27 +2,25 @@ require "spec_helper"
 require "mini_portile"
 
 describe MiniPortile do
-  let(:recipe) { MiniPortile.new("myapp", "1.2.3") }
+  let(:logger) { Support::BlackHole.new }
 
   describe "#new" do
     it "accepts additional options during initialization" do
-      logger = Support::BlackHole.new
       recipe = MiniPortile.new("myapp", "1.2.3", :logger => logger)
       recipe.logger.should eql(logger)
     end
   end
 
   describe "#download" do
-    let(:url) { "http://myserver.com/myapp-1.2.3.tar.gz" }
+    let(:recipe) { MiniPortile.new("amhello", "1.0", :logger => logger) }
+    let(:url) { fixture_file("amhello-1.0.tar.gz") }
     in_temporary_directory
 
     before :each do
       recipe.files << url
-
-      FakeWeb.register_uri(:get, url, :body => "FIXME", :content_length => 5)
     end
 
-    it "downloads the files indicated" do
+    it "downloads the indicated files" do
       recipe.download
       FakeWeb.should have_requested(:get, url)
     end
--- spec/support/download_helper.rb
+++ spec/support/download_helper.rb
@@ -6,10 +6,26 @@ FakeWeb.allow_net_connect = false
 
 module Support
   module DownloadHelper
+    FIXTURES_ROOT = File.expand_path("../fixtures", File.dirname(__FILE__))
+
     def self.included(example_group)
       example_group.after :each do
         FakeWeb.clean_registry
       end
+    end
+
+    def fixture_file(filename)
+      # build URL for requested file
+      url = "http://server.com/pub/%s" % filename
+
+      # read
+      file_path = File.join(FIXTURES_ROOT, filename)
+      body = File.open(file_path, "rb") { |f| f.read }
+
+      # register download for it
+      FakeWeb.register_uri(:get, url, :body => body, :content_length => body.length)
+
+      url
     end
   end
 end

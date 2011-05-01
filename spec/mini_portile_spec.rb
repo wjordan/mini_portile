2c4bb7d921558cc59bdc5a8117cadf5caf7fcdd0
Luis Lavena <luislavena@gmail.com>
2011-04-30 22:13:35 -0300

    Grammar?

--- spec/mini_portile_spec.rb
+++ spec/mini_portile_spec.rb
@@ -14,12 +14,12 @@ describe MiniPortile do
       recipe.files << url
     end
 
-    it "downloads the indicated files" do
+    it "download the indicated file" do
       recipe.download
       FakeWeb.should have_requested(:get, url)
     end
 
-    it "places the downladed file in archives directory" do
+    it "place the downladed file in archives directory" do
       recipe.download
       archives = Dir.glob("ports/archives/*.*")
       archives.should include("ports/archives/amhello-1.0.tar.gz")
